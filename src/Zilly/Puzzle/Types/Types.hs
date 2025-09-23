cc0d5ea473b7172269e3f9ed9cb3ba65907e555d
initial-mockingbird <daniel.andres.pinto@gmail.com>
2025-09-22 22:56:37 -0400

    Revert "wip"
    This reverts commit 218aae6826d731074e8a80419bdc27035872a2dd.

--- src/Zilly/Puzzle/Types/Types.hs
+++ src/Zilly/Puzzle/Types/Types.hs
@@ -12,16 +12,14 @@ type Name  = Text
 
 newtype TVar  = TV Name deriving (Eq,Ord)
 
+instance IsString TVar where
+  fromString = TV . fromString
+
 data Types
   = TCon Name [Types]
   | TVar TVar
   | TFamApp Name Types [Types]
   deriving (Eq,Ord)
-
-instance IsString TVar where
-  fromString = TV . fromString
-
-
 
 instance IsString Types where
   fromString = TVar . fromString
